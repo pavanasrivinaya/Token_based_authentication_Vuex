e591f14b346dc658e574968d59e61c3888b7a884
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-11-24 11:00:51 +0530

    first

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,47 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
+import Router from "vue-router";
 import Home from "../views/Home.vue";
+import Dashboard from "../views/Dashboard.vue";
+import RegisterUser from "../views/RegisterUser.vue";
+import LoginUser from "../views/LoginUser.vue";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: Dashboard,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterUser
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginUser
+    }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem("user");
+
+  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
+    next("/");
+  }
+  next();
 });
 
 export default router;
